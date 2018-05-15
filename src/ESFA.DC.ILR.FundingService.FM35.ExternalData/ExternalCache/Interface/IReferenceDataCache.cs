f2453dc9b533764b44d856974189effee31d2032
Ashley Beecham <ash@momentoussolutions.co.uk>
2018-05-15 09:57:01 +0100

    50109 - LARS Reference Data Service Added with Reference Data Cache. Unit tests started.

---
+++ src/ESFA.DC.ILR.FundingService.FM35.ExternalData/ExternalCache/Interface/IReferenceDataCache.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using ESFA.DC.ILR.FundingService.FM35.ExternalData.LARS.Model;
+
+namespace ESFA.DC.ILR.FundingService.FM35.ExternalData.ExternalCache.Interface
+{
+    public interface IReferenceDataCache
+    {
+        IDictionary<string, IEnumerable<LARSFunding>> LARSFunding { get; }
+
+        IDictionary<string, LARSLearningDelivery> LARSLearningDelivery { get; }
+
+        IDictionary<string, IEnumerable<LARSAnnualValue>> LARSAnnualValue { get; }
+
+        IDictionary<string, IEnumerable<LARSFrameworkAims>> LARSFrameworkAims { get; }
+
+        IDictionary<string, IEnumerable<LARSLearningDeliveryCategory>> LARSLearningDeliveryCatgeory { get; }
+
+        string LARSCurrentVersion { get; }
+
+        //IDictionary<string, IEnumerable<SfaAreaCost>> SfaAreaCost { get; }
+
+        //string PostcodeCurrentVersion { get; }
+
+        //ORG
+
+        //LEMP
+    }
+}
