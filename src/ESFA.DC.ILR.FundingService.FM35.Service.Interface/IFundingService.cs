548758694910e0778786df5a210c0dda35958b3a
Ashley Beecham <ash@momentoussolutions.co.uk>
2018-06-05 13:14:24 +0100

    51549 - Progressing with Funding Service projects

---
+++ src/ESFA.DC.ILR.FundingService.FM35.Service.Interface/IFundingService.cs
@@ -0,0 +1,12 @@
+﻿//using System.Collections.Generic;
+//using ESFA.DC.ILR.FundingService.FM35.FundingOutput.Model.Interface;
+//using ESFA.DC.ILR.Model.Interface;
+//using ESFA.DC.OPA.Model.Interface;
+
+//namespace ESFA.DC.ILR.FundingService.FM35.Service.Interface
+//{
+//    public interface IFundingService
+//    {
+//        IEnumerable<IFundingOutputs> ProcessFunding(int ukprn, IList<ILearner> learnerList);
+//    }
+//}
